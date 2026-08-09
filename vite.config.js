d58d0b8f95f768e5efbbbbf4f03fdc7d4c6b88b8
liujunxiang <jliu0457@student.monash.edu>
2026-08-09 18:37:22 +0800

    Add GitHub Pages deploy config and CI/CD workflow

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production'
+    ? '/FIT5032_2025_-Junxiang-Liu-_-36667560-/'
+    : '/',
   plugins: [
     vue(),
     vueDevTools(),
